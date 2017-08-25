5c7ae44916b43c5f8dadf15aa85cccf3402eb916
Adam Binnersley <abinnersley@gmail.com>
2017-08-25 14:58:42 +0100

    Optimise error message return

--- src/ImageUpload.php
+++ src/ImageUpload.php
@@ -321,11 +321,14 @@ class ImageUpload implements UploadInterface{
      * @return string Returns the error message
      */
     public function getErrorMsg() {
-        if($this->errorNo == 1){return 'The image is not a valid image format';}
-        elseif($this->errorNo == 2){return 'The image is too large to upload please make sure your image is smaller than '. number_format(($this->maxFileSize / 100000), 2).'MB in size your image is '.$this->imageSize;}
-        elseif($this->errorNo == 3){return 'The image is not allowed! Please make sure your image has of of the allowed extensions';}
-        elseif($this->errorNo == 4){return 'The image with this name has already been uploaded or already exists on our server!';}
-        elseif($this->errorNo == 5){return 'The image dimensions are too small. It must be greater than 200px in width and 150px in height';}
-        else{return 'An error occured while adding the image. Please try again!';}
+        $errors = array(
+            0 => 'An error occured while adding the image. Please try again!',
+            1 => 'The image is not a valid image format',
+            2 => 'The image is too large to upload please make sure your image is smaller than '. number_format(($this->maxFileSize / 100000), 2).'MB in size your image is '.$this->imageSize,
+            3 => 'The image is not allowed! Please make sure your image has of of the allowed extensions',
+            4 => 'The image with this name has already been uploaded or already exists on our server!',
+            5 => 'The image dimensions are too small. It must be greater than 200px in width and 150px in height'
+        );
+        return $errors[intval($this->errorNo)];
     }
 }
